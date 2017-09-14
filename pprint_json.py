217557d67b8a31894e7d204017e0c924e1c3bd5c
lucky <lucky@lucky.ilbirs.com>
2017-09-15 01:06:57 +0300

    naming

--- pprint_json.py
+++ pprint_json.py
@@ -6,11 +6,11 @@ import pprint
 def load_data(filepath):
     with open(filepath, 'r') as json_file:
         json_obj = json.load(json_file)
-    return json_obj
+        return json_obj
 
 
-def pretty_print_json(data):
-    pprint.pprint(data, indent=4)
+def pretty_print_json(json_for_print):
+    pprint.pprint(json_for_print, indent=4)
 
 
 if __name__ == '__main__':
